f38e5fc2746fbea61f76b33c5bd29028bad9ade8
anya827982-spec <anya827982@gmail.com>
2026-06-27 20:49:46 +0530

    Day-27 assignment is uploaded.

---
+++ Week-4/Day-27/q-105.c
@@ -0,0 +1,34 @@
+// Write a program to Create student record management system.//
+#include <stdio.h>
+struct Student
+{
+    int roll;
+    char name[20];
+    float marks;
+};
+int main()
+{
+    struct Student s[5];
+    int n, i;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        printf("\nEnter details of student %d\n", i + 1);
+        printf("Roll No: ");
+        scanf("%d", &s[i].roll);
+        printf("Name: ");
+        scanf("%s", s[i].name);
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    printf("\nStudent Records:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("\nRoll No: %d", s[i].roll);
+        printf("\nName: %s", s[i].name);
+        printf("\nMarks: %.2f\n", s[i].marks);
+    }
+    return 0;
+}
